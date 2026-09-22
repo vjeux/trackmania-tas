8dba41f15b90b3f8c1dbc1aaaa6c4b0f52306bd2
agentcloud (vjeux) <vjeux@fb.com>
2026-09-21 23:59:54 -0700

    mapgeom examples/pakcat: DataStore::read hands out an Arc now

--- tools/mapgeom/examples/pakcat.rs
+++ tools/mapgeom/examples/pakcat.rs
@@ -3,6 +3,6 @@ fn main() {
     let mut st = mapgeom::store::DataStore::empty();
     st.add_pak(&a[1], &a[2]).unwrap();
     let data = st.read(&a[3]).unwrap();
-    std::fs::write(&a[4], &data).unwrap();
+    std::fs::write(&a[4], &data[..]).unwrap();
     println!("wrote {} ({} bytes)", &a[4], data.len());
 }
